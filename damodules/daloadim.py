de14b821513cc52c414fcab8e20c8e5a760c0eeb
mehmet <mehmet.zeybel@gmail.com>
2019-09-11 14:41:43 +0300

    init deepapptest with files

---
+++ damodules/daloadim.py
@@ -0,0 +1,35 @@
+import requests
+from PIL import Image
+from io import BytesIO
+def loadImage():
+	BASE_URL = "http://localhost:8080/getCurrentImageUrl/"
+
+	applicationId = "catDog"
+	# api-endpoint 
+	URL = BASE_URL + applicationId
+
+
+	# sending get request and saving the response as response object 
+	r = requests.get(url = URL) 
+	data = r.text
+	print("Response : " + data) 
+	response = requests.get(data)
+	img = Image.open(BytesIO(response.content))
+	return img
+def serveImage(changedImage):
+	BASE_URL = "http://localhost:8080/sendCurrentImageUrlToJava/"
+	image_file = BytesIO()
+	changedImage.save(image_file, "jpeg")
+	image_file.seek(0)
+	applicationId = "catDog"
+	# api-endpoint 
+	URL = BASE_URL + applicationId
+
+
+	# sending get request and saving the response as response object 
+	
+	
+	files = {'files': image_file}
+	response = requests.post(URL, files=files)
+	print(response)
+
---
+++ simple.py
@@ -0,0 +1,31 @@
+from keras.models import load_model
+from keras.preprocessing import image
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image,ImageDraw
+import requests
+from damodules import daloadim
+from io import BytesIO
+
+model = load_model('/Users/mehmet/deepappsworks/basic_classification/dogcat_model_bak.h5')
+imgOriginal = daloadim.loadImage()
+img1 = imgOriginal.resize((64, 64), Image.ANTIALIAS)
+
+
+#img1 = image.load_img('cat_image.jpeg', target_size=(64, 64))
+img = image.img_to_array(img1)
+img = img/255
+# create a batch of size 1 [N,H,W,C]
+img = np.expand_dims(img, axis=0)
+prediction = model.predict(img, batch_size=None,steps=1) #gives all class prob.
+d = ImageDraw.Draw(imgOriginal)
+if(prediction[:,:]>0.5):
+    value ='Dog :%1.2f'%(prediction[0,0])
+    d.text((0,62), value, fill=(255,0,0))
+else:
+    value ='Cat :%1.2f'%(1.0-prediction[0,0])
+    d.text((0,62), value, fill=(255,0,0))
+daloadim.serveImage(imgOriginal)
+"""plt.imshow(img1)
+plt.show()
+"""
