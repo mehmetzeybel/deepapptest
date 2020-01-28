ea5a11721803f87b04e6607ce6b0915c1940bd2e
mehmet <mehmet.zeybel@gmail.com>
2020-01-28 23:25:54 +0300

    update bug

--- damodules/daloadim.py
+++ damodules/daloadim.py
@@ -2,10 +2,10 @@ import requests
 from PIL import Image
 from io import BytesIO
 def loadImage():
-	testApp = true;
-	if(testApp){
+	testApp = True;
+	if testApp == True:
 		img = Image.open("/opt/deepapp/work/dog.jpeg")
-	}else{
+	else:
 		BASE_URL = "http://localhost:8080/getCurrentImageUrl/"
 		applicationId = "catDog"
 		# api-endpoint 
@@ -16,22 +16,25 @@ def loadImage():
 		print("Response : " + data) 
 		response = requests.get(data)
 		img = Image.open(BytesIO(response.content))
-	}
+	
 	
 	return img
 def serveImage(changedImage):
-	BASE_URL = "http://10.103.207.95:8080/sendCurrentImageUrlToJava/"
-	image_file = BytesIO()
-	changedImage.save(image_file, "jpeg")
-	image_file.seek(0)
-	applicationId = "catDog"
-	# api-endpoint 
-	URL = BASE_URL + applicationId
+	if testApp == True:
+		img = Image.open("/opt/deepapp/work/dog.jpeg")
+	else:
+		BASE_URL = "localhost:8080/sendCurrentImageUrlToJava/"
+		image_file = BytesIO()
+		changedImage.save(image_file, "jpeg")
+		image_file.seek(0)
+		applicationId = "catDog"
+		# api-endpoint 
+		URL = BASE_URL + applicationId
 
-	# sending get request and saving the response as response object 
-	
-	
-	files = {'files': image_file}
-	response = requests.post(URL, files=files)
-	print(response)
+		# sending get request and saving the response as response object 
+		
+		
+		files = {'files': image_file}
+		response = requests.post(URL, files=files)
+		print(response)
 
